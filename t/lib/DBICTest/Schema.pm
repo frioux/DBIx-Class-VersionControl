27c449174afc2815f372da1e017183afcb70e961
Jess Robinson <castaway@desert-island.me.uk>
2007-05-10 22:09:56 +0000

    Separate db for auditing in tests

--- t/lib/DBICTest/Schema.pm
+++ t/lib/DBICTest/Schema.pm
@@ -5,6 +5,7 @@ use base qw/DBIx::Class::Schema/;
 
 __PACKAGE__->load_components(qw/+DBIx::Class::Schema::Journal/);
 
+__PACKAGE__->journal_connection(['dbi:SQLite:t/var/Audit.db']);
 __PACKAGE__->journal_user(['DBICTest::Schema::Artist', {'foreign.artistid' => 'self.user_id'}]);
 
 no warnings qw/qw/;
